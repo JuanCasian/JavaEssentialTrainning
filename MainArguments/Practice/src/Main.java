bb5b02868f1c70b87b49d5831d6725cea186e290
Juan Casian <juanpedrocasian@gmail.com>
2018-07-23 11:06:05 -0700

    Adding arguments from main, practice

---
+++ MainArguments/Practice/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String args[]){
+        // Foe each structure which goes through all the array and prints all the values
+        for (String s :
+                args) {
+            System.out.println(s);
+        }
+    }
+}
