da4fe0f44a8745eaff8af1edffea4b33b44320f7
Juan Casian <juanpedrocasian@gmail.com>
2018-07-24 08:13:50 -0700

    Adding simple calculator

--- MainArguments/Practice/src/Main.java
+++ MainArguments/Practice/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String args[]){
-        // Foe each structure which goes through all the array and prints all the values
+        // For each structure which goes through all the array and prints all the values
         for (String s :
                 args) {
             System.out.println(s);
